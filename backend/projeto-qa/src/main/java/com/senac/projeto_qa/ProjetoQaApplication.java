0b3aa7b742f85758b9f8d75aab8e8ed9924b155b
Alexsander Sampaio <alexsandersampaiolima12@gmail.com>
2026-04-05 20:35:56 -0300

    feat: Inicializando projeto

---
+++ backend/projeto-qa/src/main/java/com/senac/projeto_qa/ProjetoQaApplication.java
@@ -0,0 +1,13 @@
+package com.senac.projeto_qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoQaApplication.class, args);
+	}
+
+}
---
+++ backend/projeto-qa/src/test/java/com/senac/projeto_qa/ProjetoQaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.projeto_qa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
